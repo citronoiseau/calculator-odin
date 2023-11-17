7776b75ff313d8044aef6fc1d5f81774d9fc97c8
citronoiseau <avroraka2004@gmail.com>
2023-11-17 21:30:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let firstNum = 5;
 let secondNum = 10;
 let operator = "*";
-let calculator = new Calculator();
+
 function Calculator() {
   this.methods = {
     "-": (a, b) => a - b,
@@ -9,14 +9,12 @@ function Calculator() {
     "*": (a, b) => a * b,
     "/": (a, b) => a / b,
   };
-  this.calculate = function (firstNum, secondNum, operator) {
-    a = firstNum;
-    b = secondNum;
-    op = operator;
+  this.calculate = function (a, b, op) {
     if (!this.methods[op] || isNaN(a) || isNaN(b)) {
       return NaN;
     }
     return this.methods[op](a, b);
   };
 }
+let calculator = new Calculator();
 console.log(calculator.calculate(firstNum, secondNum, operator));
