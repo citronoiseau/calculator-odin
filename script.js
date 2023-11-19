4a39bb9fac7f2b67e461d78ccaf40760c9e3654a
citronoiseau <avroraka2004@gmail.com>
2023-11-20 00:37:01 +0200

    fully finished calculator's logic

--- script.js
+++ script.js
@@ -1,13 +1,99 @@
-let firstNum = 5;
-let secondNum = 10;
-let operator = "*";
+let firstNum = ``;
+let secondNum = ``;
+let operator = null;
+const numberBtns = document.querySelectorAll(`.number`);
+const operatorBtns = document.querySelectorAll(`.operator`);
+const currentOperation = document.querySelector(`.currentOperation`);
+const cursorElement = document.querySelector(".cursor");
+const previousOperation = document.querySelector(`.previousOperation`);
+previousOperation.textContent = ``;
+const equalsBtn = document.querySelector(`.equalsBtn`);
+const clearBtn = document.querySelector(`.clearBtn`);
+const eraseBtn = document.querySelector(`.eraseBtn`);
+
+function getNumber(number) {
+  console.log("Number:", number);
+  if (cursorElement) {
+    cursorElement.remove();
+  }
+  if (number === "." && currentOperation.textContent.includes(`.`)) {
+    return;
+  }
+  currentOperation.textContent += number;
+}
+
+numberBtns.forEach((button) =>
+  button.addEventListener(`click`, (event) =>
+    getNumber(event.target.textContent)
+  )
+);
+
+function getOperator(op) {
+  if (operator != null) getExpression();
+  console.log("Operator:", op);
+  firstNum = parseFloat(currentOperation.textContent);
+  if (isNaN(firstNum)) {
+    console.log("Invalid number");
+    return;
+  }
+  operator = op;
+  currentOperation.textContent = ``;
+  previousOperation.textContent = `${firstNum} ${operator}`;
+  currentOperation.appendChild(cursorElement);
+}
+
+operatorBtns.forEach((button) =>
+  button.addEventListener(`click`, (event) =>
+    getOperator(event.target.textContent)
+  )
+);
+
+function getExpression() {
+  secondNum = parseFloat(currentOperation.textContent);
+  if (isNaN(secondNum)) {
+    console.log("Invalid number");
+    return;
+  }
+  let result = calculator.operate(firstNum, secondNum, operator);
+  currentOperation.textContent = result;
+  previousOperation.textContent = ``;
+  firstNum = result;
+  operator = null;
+}
+equalsBtn.addEventListener(`click`, getExpression);
+
+function erase() {
+  const lastOperator = previousOperation.textContent.slice(-1);
+  if (["+", "-", "×", "÷", "^", "%"].includes(lastOperator)) {
+    operator = null;
+    previousOperation.textContent = previousOperation.textContent.slice(0, -1);
+    currentOperation.textContent = previousOperation.textContent;
+    previousOperation.textContent = ``;
+  }
+  currentOperation.textContent = currentOperation.textContent.slice(0, -1);
+  if (currentOperation.textContent === ``) {
+    currentOperation.appendChild(cursorElement);
+  }
+}
+eraseBtn.addEventListener(`click`, erase);
+function clearScreen() {
+  firstNum = ``;
+  secondNum = ``;
+  operator = null;
+  currentOperation.textContent = ``;
+  currentOperation.appendChild(cursorElement);
+  previousOperation.textContent = ``;
+}
+clearBtn.addEventListener(`click`, clearScreen);
 
 function Calculator() {
   this.methods = {
     "-": (a, b) => a - b,
     "+": (a, b) => a + b,
-    "*": (a, b) => a * b,
-    "/": (a, b) => a / b,
+    "×": (a, b) => a * b,
+    "÷": (a, b) => a / b,
+    "^": (a, b) => Math.pow(a, b),
+    "%": (a, b) => (a / 100) * b,
   };
   this.operate = function (a, b, op) {
     if (!this.methods[op] || isNaN(a) || isNaN(b)) {
